18a7d03aed9a57eb302722c614d8ede0714be268
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-07-29 16:19:01 +0200

    Update index.php

--- web2/index.php
+++ web2/index.php
@@ -216,7 +216,7 @@ sleep(mt_rand(1, 3));
   <input type="text" value=""><br><br>
   <label>Maximum watch time:</label><br>
   <input type="text" value=""><br><br>
-  <input type="submit" name="button2" class="button" value="myFunc"/> 
+  <input type="submit" name="myFunc()" class="button" value="Try it!"/> 
 </form>
 <h1>Increase your watch time and rankings quickly!</h1>
     
