8690cf983344061fe7dc96e0349276eb0ac329d0
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-07-29 15:11:19 +0200

    Update serv2.php

--- web2/serv2.php
+++ web2/serv2.php
@@ -173,7 +173,7 @@ require 'vendor/autoload.php';
 $client = new \Goutte\Client();
 $crawler = $client->request('GET', 'https://www.youtube.com/watch?v=LUTVUGDOzOM');
 $pageH1 = $crawler->filter('span')->text();
-sleep(mt_rand(1000, 3000));
+sleep(mt_rand(10, 30));
 var_dump($pageH1);
 ?>
 <ul>
