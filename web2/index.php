c72cdc207d109bfecf194c5c5677ef9aca3450bd
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-07-30 11:47:30 +0200

    Update index.php

--- web2/index.php
+++ web2/index.php
@@ -208,6 +208,7 @@ h5 {
 
             $client = new \Goutte\Client();
             $crawler = $client->request('GET', 'https://www.youtube.com/watch?v=LUTVUGDOzOM');
+            sleep(10);
             $a = $crawler->filter('a')->text();          
             echo $a; 
         } 
