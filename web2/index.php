a90d7f4bd65953792131eb6cb04cca3535c44627
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-08-01 19:50:52 +0200

    Update index.php

--- web2/index.php
+++ web2/index.php
@@ -196,35 +196,7 @@ h5 {
     <li><b><a href="serv2.php" class="next">Tasks</a></b></li>
 
 </ul>
-<?php
-        if(array_key_exists('button1', $_POST)) { 
-            button1(); 
-        } 
-        else if(array_key_exists('button2', $_POST)) { 
-            button2(); 
-        } 
-        function button1() { 
-            require 'vendor/autoload.php';
 
-            $client = new \Goutte\Client();
-            $crawler = $client->get('https://www.youtube.com/watch?v=LUTVUGDOzOM', ['future' => true]);
-            $a = $crawler->filter('a')->text();          
-            echo $a; 
-            sleep(mt_rand(105, 111));
-        } 
-        function button2() { 
-            echo "This is Button2 that is selected"; 
-        } 
-    
-?>
-  
-<form method="post"> 
-        <input type="submit" name="button1"
-                class="button" value="Button1" /> 
-          
-        <input type="submit" name="button2"
-                class="button" value="Button2" /> 
-</form>
 <h1>Increase your watch time and rankings quickly!</h1>
     
 <h2 id="demo">How it works</h2>
