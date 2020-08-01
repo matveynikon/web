df69bc7d0b17baad01f570de626749929b2b336f
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-08-01 19:51:23 +0200

    Update serv2.php

--- web2/serv2.php
+++ web2/serv2.php
@@ -6,13 +6,33 @@
 </head>
 <body>
 <?php
-require 'vendor/autoload.php';
+        if(array_key_exists('button1', $_POST)) { 
+            button1(); 
+        } 
+        else if(array_key_exists('button2', $_POST)) { 
+            button2(); 
+        } 
+        function button1() { 
+            require 'vendor/autoload.php';
 
-$client = new \Goutte\Client();
-$crawler = $client->request('GET', 'https://www.youtube.com/watch?v=LUTVUGDOzOM');
-$pageH1 = $crawler->filter('span')->text();
-sleep(mt_rand(10, 30));
-var_dump($pageH1);
+            $client = new \Goutte\Client();
+            $crawler = $client->get('https://www.youtube.com/watch?v=LUTVUGDOzOM', ['future' => true]);
+            $a = $crawler->filter('a')->text();          
+            echo $a; 
+            sleep(mt_rand(105, 111));
+        } 
+        function button2() { 
+            echo "This is Button2 that is selected"; 
+        } 
+    
 ?>
+  
+<form method="post"> 
+        <input type="submit" name="button1"
+                class="button" value="Button1" /> 
+          
+        <input type="submit" name="button2"
+                class="button" value="Button2" /> 
+</form>
 </body>
 </html>
