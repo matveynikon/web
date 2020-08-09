6fdfaab7ff774eaebcb453ddbc9b7c8bbd1be120
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-08-09 12:55:30 +0200

    Update serv2.php

--- web2/serv2.php
+++ web2/serv2.php
@@ -6,32 +6,20 @@
 </head>
 <body>
 <?php
-        if(array_key_exists('button1', $_POST)) { 
-            button1(); 
-        } 
-        else if(array_key_exists('button2', $_POST)) { 
-            button2(); 
-        } 
-        function button1() { 
-            require 'vendor/autoload.php';
 
-            $client = new \Goutte\Client();
-            $crawler = $client->get('https://www.youtube.com/watch?v=LUTVUGDOzOM');
-            $button = $crawler->selectButton('...')->button();            
-            sleep(mt_rand(1, 5));
-        } 
-        function button2() { 
-            echo "This is Button2 that is selected"; 
-        } 
-    
+class FirstCest
+{
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+        $I->click('#toggleButton');
+    }
+}
 ?>
   
 <form method="post"> 
-        <input type="submit" name="button1"
+        <input type="submit" name="FirstCest"
                 class="button" value="Button1" /> 
           
-        <input type="submit" name="button2"
-                class="button" value="Button2" /> 
 </form>
 </body>
 </html>
